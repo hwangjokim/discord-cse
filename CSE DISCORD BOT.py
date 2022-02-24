232bd9b85709366881cf54683af05c547c1b2a40
hwangjokim <hg7258@naver.com>
2022-02-24 18:44:44 +0900

    sadas

--- CSE DISCORD BOT.py
+++ CSE DISCORD BOT.py
@@ -130,6 +130,7 @@ async def infoMoney(ctx, user: discord.User):
         print("")
         await ctx.send("해당 회원은 등록되지 않았습니다.")
 
+
 #====================================================================================================#
 # 추첨 기능 
 @commands.has_role("추첨자")
@@ -150,7 +151,7 @@ async def shuffle(ctx):   #box = 추첨을 위해 미리 만들어둔 리스트
 @client.command(name='추첨')
 async def raffle(ctx):
     if box: 
-        await ctx.send(f'당첨자 : {box[0]}') #0번째 인덱스를 당첨자로 선택
+        await ctx.send(f'당첨자 : {box[0]} (보유 추첨권 : {box.count(box[0])}') #0번째 인덱스를 당첨자로 선택
         temp=box[0]
         #print(temp)
         while temp in box:  
